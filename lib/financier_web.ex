b80106a5390d67e0e60b2016feb045384db078f9
fredenga <dengafred@gmail.com>
2025-04-11 11:41:18 -0700

    create main_live_view

--- lib/financier_web.ex
+++ lib/financier_web.ex
@@ -59,6 +59,15 @@ defmodule FinancierWeb do
     end
   end
 
+  def main_live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {FinancierWeb.Layouts, :main}
+
+        unquote(html_helpers())
+    end
+  end
+
   def live_component do
     quote do
       use Phoenix.LiveComponent
