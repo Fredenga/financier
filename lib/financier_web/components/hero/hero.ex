701c891ed1884652f23cc6e430017fa9b7fc7f47
fredenga <dengafred@gmail.com>
2025-04-14 02:52:54 -0700

    add hero image section

--- lib/financier_web/components/hero/hero.ex
+++ lib/financier_web/components/hero/hero.ex
@@ -5,7 +5,7 @@ defmodule FinancierWeb.Hero.Hero do
 
   def hero(assigns) do
     ~H"""
-      <div class="w-screen md:px-10 lg:px-24 flex mt-10">
+      <div class="w-screen md:px-10 lg:px-24 flex flex-col md:flex-row mt-10">
         <.hero_text />
         <.hero_img />
       </div>
--- lib/financier_web/components/hero/hero_img.ex
+++ lib/financier_web/components/hero/hero_img.ex
@@ -1,10 +1,10 @@
 defmodule FinancierWeb.Hero.HeroImg do
   use Phoenix.Component
-  
+
   def hero_img(assigns) do
     ~H"""
-      <div class="w-1/2">
-        <h1>Welcome Home Hero Img</h1>
+      <div class="w-1/2 flex items-center justify-center">
+        <img src="/images/hero-img.jpg" alt="hero-img" class="h-[55vh] w-[100%] object-contain" />
       </div>
     """
   end
