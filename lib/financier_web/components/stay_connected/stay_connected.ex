032c8d7f42f8ce779107929936046bf0e494df52
fredenga <dengafred@gmail.com>
2025-04-21 12:43:45 -0700

    stay connected section complete

--- lib/financier_web/components/stay_connected/stay_connected.ex
+++ lib/financier_web/components/stay_connected/stay_connected.ex
@@ -5,13 +5,14 @@ defmodule FinancierWeb.StayConnected.StayConnected do
     ~H"""
     <div class="px-24 mt-20 flex flex-col items-center">
       <.stay_connected_header />
+      <.actions />
     </div>
     """
   end
 
   defp stay_connected_header(assigns) do
     ~H"""
-    <div class="mb-20">
+    <div class="">
       <div class="flex flex-col items-center justify-center w-full">
         <h1 class="text-5xl leading-normal font-[600] text-center max-w-xl">
           Stay Connected, Manage Finances Smarter.
@@ -21,6 +22,15 @@ defmodule FinancierWeb.StayConnected.StayConnected do
         </p>
       </div>
     </div>
+    """
+  end
+
+  defp actions(assigns) do
+    ~H"""
+    <button class="flex mt-10 space-x-3 bg-blue-500 text-white px-4 py-2 rounded-full ">
+      <p class="text-xl">Try for Free</p>
+      <i class="fas fa-arrow-right text-blue-500 bg-white text-3xl p-2 rounded-full"></i>
+    </button>
     """
   end
 end
