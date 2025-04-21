7e76248631f1e380323a2c3e858ed709cb7cbba1
fredenga <dengafred@gmail.com>
2025-04-21 12:07:51 -0700

    make user views cards

--- lib/financier_web/components/user_views/user_views.ex
+++ lib/financier_web/components/user_views/user_views.ex
@@ -5,6 +5,7 @@ defmodule FinancierWeb.UserViews.UserViews do
     ~H"""
     <div class="px-24 mt-20 flex flex-col items-center">
       <.user_views_header />
+      <.user_views_cards />
     </div>
     """
   end
@@ -24,4 +25,23 @@ defmodule FinancierWeb.UserViews.UserViews do
     </div>
     """
   end
-end
+
+  defp user_views_cards(assigns) do
+    ~H"""
+      <div>
+        <div class="flex flex-col md:flex-row lg:px-40 md:px-20">
+          <div class="w-2/3 px-6">
+            <h1 class="text-2xl mb-20 text-blue-500">verse</h1>
+            <h2 class="text-lg mb-5 font-semibold">David Emily</h2>
+            <p>
+              I used to struggle with tracking my expenses and always wondered where my money went at the end of the month.
+              Since I started using this app, I have complete visibility into my spending habits. The AI-powered insights have helped me identify!"
+            </p>
+          </div>
+          <img src="/images/user_views/img1.jpg" alt="User Image" class="w-[40vh] object-contain rounded-xl" />
+        </div>
+      </div>
+    """
+    end
+
+  end
