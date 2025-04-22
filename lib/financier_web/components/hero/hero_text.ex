55f532d251125dd51625e054a45206c069096843
fredenga <dengafred@gmail.com>
2025-04-22 13:45:50 -0700

    add navigate to contact from hero

--- lib/financier_web/components/hero/hero_text.ex
+++ lib/financier_web/components/hero/hero_text.ex
@@ -1,5 +1,6 @@
 defmodule FinancierWeb.Hero.HeroText do
   use Phoenix.Component
+  use FinancierWeb, :html
 
   def hero_text(assigns) do
     ~H"""
@@ -7,30 +8,32 @@ defmodule FinancierWeb.Hero.HeroText do
       <h1 class="text-5xl leading-normal font-[600]">
         Effortlessly Organize and Simplify Your Finances
       </h1>
-      
+
       <p class="text-md text-gray-500">
         Automatically track your expenses, set budgets, and achieve your financial goals with AI-powered insights. Your money, your rules — track, save, and grow.
       </p>
-      
+
       <div class="flex items-center w-full mt-10">
+      <.link patch={~p"/contact"}>
         <button class="flex  space-x-3 bg-blue-500 text-white px-4 py-2 rounded-full">
           <p class="text-xl">Try for Free</p>
            <i class="fas fa-arrow-right text-blue-500 bg-white text-3xl p-2 rounded-full"></i>
         </button>
-        
+      </.link>
+
         <button class="flex space-x-2 bg-transparent px-3 py-1 rounded-full">
           <i class="fas fa-arrow-right bg-blue-500 text-white text-3xl p-2 rounded-full"></i>
           <p class="text-lg text-black">Download Now</p>
         </button>
       </div>
-      
+
       <div class="flex items-center space-x-3 mt-10 pl-4">
         <div class="flex ">
           <img class="h-12 w-12 object-contain -ml-2 z-10" src="/images/hero/img1.svg" alt="avatar" />
           <img class="h-12 w-12 object-contain -ml-2 z-20" src="/images/hero/img2.svg" alt="avatar" />
           <img class="h-12 w-12 object-contain -ml-2 z-30" src="/images/hero/img3.svg" alt="avatar" />
         </div>
-        
+
         <p class="text-md text-gray-500">Trusted by +20K People.</p>
       </div>
     </div>
--- lib/financier_web/components/navbar/navbar.ex
+++ lib/financier_web/components/navbar/navbar.ex
@@ -1,5 +1,6 @@
 defmodule FinancierWeb.Navbar.Navbar do
   use Phoenix.Component
+  
 
   def main_nav(assigns) do
     links = ["Home", "About", "Features", "Pricing"]
@@ -10,7 +11,7 @@ defmodule FinancierWeb.Navbar.Navbar do
       <div class="w-1/3 text-center">
         <h1 class="text-2xl text-bold">Financier</h1>
       </div>
-      
+
       <div class="w-1/3">
         <ul class="flex justify-between text-xl">
           <li :for={link <- @links}>
@@ -18,7 +19,7 @@ defmodule FinancierWeb.Navbar.Navbar do
           </li>
         </ul>
       </div>
-      
+
       <div class="w-1/3 flex items-center justify-center space-x-5">
         <i class="fas fa-shopping-bag text-xl"></i>
         <button class="flex items-center justify-center space-x-3 bg-black text-white px-4 py-2 rounded-full">
